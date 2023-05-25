6237e06c252856dcf3e071e57910f9c1665ecfa2
Shivam Pravin Upadhyay <shivamupadhyay202@nhitm.ac.in>
2023-05-25 20:46:50 +0530

    Adding js

---
+++ solidity/index.js
@@ -0,0 +1,8 @@
+let a = "This is a solidity learning guide for us!";
+if (a==1) {
+    console.log("Welcome to the track")
+} else {
+    console.log("You can't be a part")
+}
+
+prompt("Enter the value of a:");
